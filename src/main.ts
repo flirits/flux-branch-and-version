c3dc01291bf5dcfbb895d238422951f17785aaf2
Van Nieuwenhuyse, Stijn <stijn.vannieuwenhuyse@flir.com>
2022-11-21 17:20:35 +0100

    Add initial Flux branch and version action

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import * as core from '@actions/core'
 
-async function run(): Promise<void> {
+function run(): void {
   try {
     const version = core.getInput('version')
-    const defaultRef = core.getInput('default-ref') ?? process.env.GITHUB_REF_NAME
-    const fluxServerRef = core.getInput('flux-server-ref') ?? defaultRef
-    const fluxHybridRef = core.getInput('flux-hybrid-ref') ?? defaultRef
-    const fluxWebRef = core.getInput('flux-web-ref') ?? defaultRef
-    const fluxStreamingServerRef = core.getInput('flux-streaming-server-ref') ?? defaultRef
+    const defaultRef = core.getInput('default-ref') || process.env.GITHUB_REF_NAME
+    if (!defaultRef) {
+      core.setFailed('Unable to retrieve the branch name')
+      return
+    }
+    const fluxServerRef = core.getInput('flux-server-ref') || defaultRef
+    const fluxHybridRef = core.getInput('flux-hybrid-ref') || defaultRef
+    const fluxWebRef = core.getInput('flux-web-ref') || defaultRef
+    const fluxStreamingServerRef = core.getInput('flux-streaming-server-ref') || defaultRef
+    const fluxProxyRef = core.getInput('flux-proxy-ref') || defaultRef
     let versionString
     if (defaultRef === 'master') {
       versionString = `${version}-b${process.env.GITHUB_RUN_NUMBER}`
@@ -26,6 +31,7 @@ async function run(): Promise<void> {
     core.info(`flux-hybrid-ref: ${fluxHybridRef}`)
     core.info(`flux-web-ref: ${fluxWebRef}`)
     core.info(`flux-streaming-server-ref: ${fluxStreamingServerRef}`)
+    core.info(`flux-proxy-ref: ${fluxProxyRef}`)
 
     core.setOutput('version-string', versionString)
     core.setOutput('default-ref', defaultRef)
@@ -33,6 +39,7 @@ async function run(): Promise<void> {
     core.setOutput('flux-hybrid-ref', fluxHybridRef)
     core.setOutput('flux-web-ref', fluxWebRef)
     core.setOutput('flux-streaming-server-ref', fluxStreamingServerRef)
+    core.setOutput('flux-proxy-ref', fluxProxyRef)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
