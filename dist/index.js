4fa7d9084233f1bbcbb7cb2fa157454cf272e8c1
Van Nieuwenhuyse, Stijn <stijn.vannieuwenhuyse@flir.com>
2022-11-21 16:45:29 +0100

    Add initial Flux branch and version action

--- dist/index.js
+++ dist/index.js
@@ -36,16 +36,40 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(186));
-const wait_1 = __nccwpck_require__(817);
 function run() {
+    var _a, _b, _c, _d, _e;
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const ms = core.getInput('milliseconds');
-            core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-            core.debug(new Date().toTimeString());
-            yield (0, wait_1.wait)(parseInt(ms, 10));
-            core.debug(new Date().toTimeString());
-            core.setOutput('time', new Date().toTimeString());
+            const version = core.getInput('version');
+            const defaultRef = (_a = core.getInput('default-ref')) !== null && _a !== void 0 ? _a : process.env.GITHUB_REF_NAME;
+            const fluxServerRef = (_b = core.getInput('flux-server-ref')) !== null && _b !== void 0 ? _b : defaultRef;
+            const fluxHybridRef = (_c = core.getInput('flux-hybrid-ref')) !== null && _c !== void 0 ? _c : defaultRef;
+            const fluxWebRef = (_d = core.getInput('flux-web-ref')) !== null && _d !== void 0 ? _d : defaultRef;
+            const fluxStreamingServerRef = (_e = core.getInput('flux-streaming-server-ref')) !== null && _e !== void 0 ? _e : defaultRef;
+            let versionString;
+            if (defaultRef === 'master') {
+                versionString = `${version}-b${process.env.GITHUB_RUN_NUMBER}`;
+            }
+            else {
+                const branchStringInLowerCase = defaultRef.toLowerCase();
+                let branchString = branchStringInLowerCase;
+                if (branchStringInLowerCase.includes('/')) {
+                    branchString = branchStringInLowerCase.substring(branchStringInLowerCase.indexOf('/') + 1).replace('/', '-');
+                }
+                versionString = `${version}-${branchString}-b${process.env.GITHUB_RUN_NUMBER}`;
+            }
+            core.info(`version-string: ${versionString}`);
+            core.info(`default-ref: ${defaultRef}`);
+            core.info(`flux-server-ref: ${fluxServerRef}`);
+            core.info(`flux-hybrid-ref: ${fluxHybridRef}`);
+            core.info(`flux-web-ref: ${fluxWebRef}`);
+            core.info(`flux-streaming-server-ref: ${fluxStreamingServerRef}`);
+            core.setOutput('version-string', versionString);
+            core.setOutput('default-ref', defaultRef);
+            core.setOutput('flux-server-ref', fluxServerRef);
+            core.setOutput('flux-hybrid-ref', fluxHybridRef);
+            core.setOutput('flux-web-ref', fluxWebRef);
+            core.setOutput('flux-streaming-server-ref', fluxStreamingServerRef);
         }
         catch (error) {
             if (error instanceof Error)
@@ -54,37 +78,6 @@ function run() {
     });
 }
 run();
-
-
-/***/ }),
-
-/***/ 817:
-/***/ (function(__unused_webpack_module, exports) {
-
-"use strict";
-
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.wait = void 0;
-function wait(milliseconds) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return new Promise(resolve => {
-            if (isNaN(milliseconds)) {
-                throw new Error('milliseconds not a number');
-            }
-            setTimeout(() => resolve('done!'), milliseconds);
-        });
-    });
-}
-exports.wait = wait;
 
 
 /***/ }),
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,38 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const version = core.getInput('version')
+    const defaultRef = core.getInput('default-ref') ?? process.env.GITHUB_REF_NAME
+    const fluxServerRef = core.getInput('flux-server-ref') ?? defaultRef
+    const fluxHybridRef = core.getInput('flux-hybrid-ref') ?? defaultRef
+    const fluxWebRef = core.getInput('flux-web-ref') ?? defaultRef
+    const fluxStreamingServerRef = core.getInput('flux-streaming-server-ref') ?? defaultRef
+    let versionString
+    if (defaultRef === 'master') {
+      versionString = `${version}-b${process.env.GITHUB_RUN_NUMBER}`
+    } else {
+      const branchStringInLowerCase = defaultRef.toLowerCase()
+      let branchString = branchStringInLowerCase
+      if (branchStringInLowerCase.includes('/')) {
+        branchString = branchStringInLowerCase.substring(branchStringInLowerCase.indexOf('/') + 1).replace('/', '-')
+      }
+      versionString = `${version}-${branchString}-b${process.env.GITHUB_RUN_NUMBER}`
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.info(`version-string: ${versionString}`)
+    core.info(`default-ref: ${defaultRef}`)
+    core.info(`flux-server-ref: ${fluxServerRef}`)
+    core.info(`flux-hybrid-ref: ${fluxHybridRef}`)
+    core.info(`flux-web-ref: ${fluxWebRef}`)
+    core.info(`flux-streaming-server-ref: ${fluxStreamingServerRef}`)
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('version-string', versionString)
+    core.setOutput('default-ref', defaultRef)
+    core.setOutput('flux-server-ref', fluxServerRef)
+    core.setOutput('flux-hybrid-ref', fluxHybridRef)
+    core.setOutput('flux-web-ref', fluxWebRef)
+    core.setOutput('flux-streaming-server-ref', fluxStreamingServerRef)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
