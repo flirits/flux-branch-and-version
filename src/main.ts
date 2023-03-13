5094a55f6f4c6d8b6b9139052c99c8807dfc9b00
Thomas Spiessens <118807360+TSPTDY@users.noreply.github.com>
2023-03-13 10:38:58 +0100

    add support for plugins

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ function run(): void {
       return
     }
     const fluxServerRef = core.getInput('flux-server-ref') || defaultRef
+    const fluxPluginsRef = core.getInput('flux-plugins-ref') || defaultRef
     const fluxHybridRef = core.getInput('flux-hybrid-ref') || defaultRef
     const fluxWebRef = core.getInput('flux-web-ref') || defaultRef
     const fluxStreamingServerRef = core.getInput('flux-streaming-server-ref') || defaultRef
@@ -32,6 +33,7 @@ function run(): void {
     core.info(`version-string: ${versionString}`)
     core.info(`default-ref: ${defaultRef}`)
     core.info(`flux-server-ref: ${fluxServerRef}`)
+    core.info(`flux-plugins-ref: ${fluxPluginsRef}`)
     core.info(`flux-hybrid-ref: ${fluxHybridRef}`)
     core.info(`flux-web-ref: ${fluxWebRef}`)
     core.info(`flux-streaming-server-ref: ${fluxStreamingServerRef}`)
@@ -43,6 +45,7 @@ function run(): void {
     core.setOutput('version-string', versionString)
     core.setOutput('default-ref', defaultRef)
     core.setOutput('flux-server-ref', fluxServerRef)
+    core.setOutput('flux-plugins-ref', fluxPluginsRef)
     core.setOutput('flux-hybrid-ref', fluxHybridRef)
     core.setOutput('flux-web-ref', fluxWebRef)
     core.setOutput('flux-streaming-server-ref', fluxStreamingServerRef)
